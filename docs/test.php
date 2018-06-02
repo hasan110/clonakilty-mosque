c4348e31d8f3e43c58d472d175cd3a3867184ec0
Hasan Raza <hasan.pakistani@gmail.com>
2018-06-01 18:12:32 -0700

    Proxy Url

--- docs/test.php
+++ docs/test.php
@@ -1,6 +1,20 @@
 <?php
-header('Content-Type', 'applicaion/json');
-//print_r($_SERVER);
-echo json_encode($_SERVER);
+$url = (isset($_GET['url'])) ? $_GET['url'] : false;
+if(!$url) exit;
 
+$referer = (isset($_SERVER['HTTP_REFERER'])) ? strtolower($_SERVER['HTTP_REFERER']) : false;
+$is_allowed = $referer && strpos($referer, strtolower($_SERVER['SERVER_NAME'])) !== false;
+$is_allowed = true;
+$string = ($is_allowed) ? utf8_encode(file_get_contents($url)) : 'You are not allowed to use this proxy!';
+$json = json_encode($string);
+
+
+$callback = (isset($_GET['callback'])) ? $_GET['callback'] : false;
+if($callback){
+	$jsonp = "$callback($json)";
+	header('Content-Type: application/javascript');
+	echo $jsonp;
+	exit;
+}
+echo $string;
 ?>
